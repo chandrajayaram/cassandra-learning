a03ea8d936b8db290e64411fdf42895714078d8d
Chandrashekar Jayaram <cjayaram@caiso.com>
2017-11-16 18:04:19 -0800

    added test infrastructure

--- killrvideo-java/src/test/java/killrvideo/service/UserManagementServiceTest.java
+++ killrvideo-java/src/test/java/killrvideo/service/UserManagementServiceTest.java
@@ -1,10 +1,19 @@
+
 package killrvideo.service;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(MvtApplicationConfig.class)
-public class UserManagementServiceTest {
+import killrvideo.configuration.KillrVideoConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={KillrVideoConfiguration.class})
+public class UserManagementServiceTest {
+	
+	@Test
+	public void testContextInitialization(){
+		
+	}
 }
