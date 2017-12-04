c16dba6b9eeb4ce201bd7ac4d04d21d87e88c2e6
chandrajayaram <chandra.jayaram.c@gmail.com>
2017-12-04 09:00:35 -0800

    adding rest layer

--- killrvideo-service/src/main/java/killrvideo/service/rest/UserResource.java
+++ killrvideo-service/src/main/java/killrvideo/service/rest/UserResource.java
@@ -1,15 +1,37 @@
 package killrvideo.service.rest;
 
+import java.util.concurrent.ExecutionException;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 import org.springframework.stereotype.Component;
 
+import killrvideo.entity.User;
+import killrvideo.entity.UserCredentials;
+import killrvideo.service.UserManagementService;
+
 @Component	
 @Path("/api/users")
 public class UserResource {
+	@Inject
+	private UserManagementService userMgmtService;
+	
 	@GET
-	public String message() {
+	public String authenticate(String userName, String password) {
 		return "Hello";
+	}
+	
+	@Path("/{userId}")
+	@GET
+	public User getProfile(String userId) {
+		try {
+			return userMgmtService.getUser(userId);
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;	
 	}
 }
